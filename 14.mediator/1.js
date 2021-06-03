5ebf8b62e2b1b09af62649fa7f2b8c662d26356b
JX-Zhuang <who.zhuang@gmail.com>
2021-06-03 21:30:42 +0800

    update

--- 14.mediator/1.js
+++ 14.mediator/1.js
@@ -11,16 +11,48 @@ Player.prototype.lose = function () {
 }
 Player.prototype.die = function () {
     this.state = 'dead';
-    playerDirector.ReceieveMessage('playerDead', this);
+    playerDirector.ReceiveMessage('playerDead', this);
 }
 Player.prototype.remove = function () {
-    playerDirector.ReceieveMessage('removePlayer', this);
+    playerDirector.ReceiveMessage('removePlayer', this);
 }
-Player.prototype.changeTeam = function () {
-    playerDirector.ReceieveMessage('changeTeam', this);
+Player.prototype.changeTeam = function (color) {
+    playerDirector.ReceiveMessage('changeTeam', this, color);
 }
 var playerFactory = function (name, team) {
     var player = new Player(name, team);
-    playerDirector.ReceieveMessage('addPlayer', player);
+    playerDirector.ReceiveMessage('addPlayer', player);
     return player;
 }
+var playerDirector = (function () {
+    var players = {},
+        operations = {};
+    operations.addPlayer = function (player) {
+        var team = player.team;
+        players[team] = players[team] || [];
+        players[team].push(player);
+    };
+    operations.removePlayer = function (player) {
+        var team = player.team,
+            teamPlayers = players[team] || [];
+        for (var i = 0; i < teamPlayers.length; i++) {
+            if (teamPlayers[i] === player) {
+                teamPlayers.splice(i, 1);
+            }
+        }
+    };
+    operations.changeTeam = function (player, color) {
+        operations.removePlayer(player);
+        player.team = color;
+        operations.addPlayer(player);
+    };
+    operations.playerDead = function () {
+
+    };
+    var ReceiveMessage = function (message, ...argus) {
+        operations[message].apply(this, argus);
+    };
+    return {
+        ReceiveMessage
+    };
+})();
