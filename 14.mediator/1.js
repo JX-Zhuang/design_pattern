b419d1beead511eda572eb9252e5ea330e9d7e0d
JX-Zhuang <who.zhuang@gmail.com>
2021-06-04 13:29:42 +0800

    mediator

--- 14.mediator/1.js
+++ 14.mediator/1.js
@@ -46,8 +46,29 @@ var playerDirector = (function () {
         player.team = color;
         operations.addPlayer(player);
     };
-    operations.playerDead = function () {
-
+    operations.playerDead = function (player) {
+        var team = player.team,
+            teamPlayers = players[team];
+        var all_dead = true;
+        for (const item of teamPlayers) {
+            if (item.state !== 'dead') {
+                all_dead = false;
+                break;
+            }
+        }
+        if (all_dead) {
+            for (const item of teamPlayers) {
+                item.lose();
+            }
+            for (var color in players) {
+                if (color !== team) {
+                    var teamPlayers = players[color];
+                    for (const item of teamPlayers) {
+                        item.win();
+                    }
+                }
+            }
+        }
     };
     var ReceiveMessage = function (message, ...argus) {
         operations[message].apply(this, argus);
@@ -56,3 +77,24 @@ var playerDirector = (function () {
         ReceiveMessage
     };
 })();
+
+var p1 = playerFactory('皮蛋', 'red'),
+    p2 = playerFactory('小乖', 'red'),
+    p3 = playerFactory('小强', 'red');
+
+
+var p4 = playerFactory('小明', 'blue'),
+    p5 = playerFactory('葱头', 'blue'),
+    p6 = playerFactory('胖墩', 'blue');
+
+// p1.die();
+// p2.die();
+// p3.die();
+
+// p1.remove();
+// p2.remove();
+// p3.die();
+
+p1.changeTeam('blue');
+p2.die();
+p3.die();
